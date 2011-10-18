c27c347ac31bced5781c27515c30022bdeebfd58
me <me@me-VirtualBox.(none)>
2011-10-18 16:49:39 -0400

    Attempt

--- Project/app/models/trading_party.rb
+++ Project/app/models/trading_party.rb
@@ -1,6 +1,7 @@
 class TradingParty < ActiveRecord::Base
 
     has_many :items
-    validates_presence_of :items, :money
+    validates_presence_of :items
+    accepts_nested_attributes_for :travelers
 
 end
